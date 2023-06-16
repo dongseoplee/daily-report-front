05660719ab45760aca3c433ec4c4a8575b08e404
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-06-17 01:14:03 +0900

    add comment textfield

--- src/Borad/Board.js
+++ src/Borad/Board.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import "./Board.scss";
 import Comment from "../components/Comment";
+import CommentInput from "../components/CommentInput";
+import defaultImage from "../images/noimage.png"
 
 const Board = () => {
     //url 파라미터 받기
@@ -48,7 +50,13 @@ const Board = () => {
           <div className="board-body">
             <div className="board-image">
               {/* <img src={"https://source.unsplash.com/random/300x300/?korea"}/> */}
-              <img src={boardData.image} width={300} height={300}/>
+              {
+                boardData.image ? (
+                  <img src={boardData.image} width={600} height={300} />
+                ) : (
+                  <img src={defaultImage} width={600} height={350} />
+
+              )}
           </div>
           <div className="board-title-content">
             <div className="board-title">{boardData.title}</div>
@@ -56,9 +64,14 @@ const Board = () => {
           </div>
         </div>
         <hr/>
+
+          <div>
+            <CommentInput board_id={boardData.id}/>
+          </div>
           <div>
             {commentData.map((item) => (
               <Comment key={item.id} 
+                board_id={item.board_id}
                 username={item.username}
                 yyyymmdd={item.yyyymmdd}
                 content={item.content}
