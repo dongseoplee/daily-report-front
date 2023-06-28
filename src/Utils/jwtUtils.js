c65dae62f5006ce33cc21762eb666141416e703a
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-06-28 18:33:33 +0900

    add logout button

--- src/Utils/jwtUtils.js
+++ src/Utils/jwtUtils.js
@@ -19,4 +19,6 @@ export class jwtUtils {
     return decoded.sub;
   }
 
+
+
 }
--- src/redux/reducers/AuthReducer.js
+++ src/redux/reducers/AuthReducer.js
@@ -9,14 +9,28 @@ export const setToken = (token) => ({
   token
 })
 
+//여기서 부터 이해가 필요함...
+export const logout = () => {
+  return {
+    type: 'LOGOUT',
+  };
+};
+
 export const AuthReducer = (state = AuthInitialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case SET_TOKEN:
       return {
         ...state,
         token: action.token
-      }
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        token: null,
+      };
     default:
       return state;
   }
 }
+
+
